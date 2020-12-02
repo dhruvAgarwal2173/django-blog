92185b9d7fa0ef39605590a2d1f5e40f8e6e8c0a
Dhruv <dhruv.agarwal2173@gmail.com>
2020-12-02 23:54:39 +0530

    Fixed 'last_modified' field in 'blog/models.py' and removed the redundant 'published_date'.

--- blog/models.py
+++ blog/models.py
@@ -8,17 +8,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=180, null=False)
     text = tinymce.models.HTMLField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField()
-    last_modified_date = models.DateTimeField()
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def last_modified(self):
-        self.last_modified_date = timezone.now()
-        self.save()
+    created_date = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modified_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title, self.author
