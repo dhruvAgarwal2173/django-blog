ce82a8cbd3e193bed7d8f821b0db5045a1cbb89b
Dhruv <dhruv.agarwal2173@gmail.com>
2020-12-03 00:15:12 +0530

    Added routing to Blog Home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(last_modified__lte=timezone.now()).order_by('last_modified')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
