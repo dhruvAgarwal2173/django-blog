6f093d78648c6403cd3d47835101418a0368c25b
Dhruv <dhruv.agarwal2173@gmail.com>
2020-12-02 23:06:18 +0530

    Initial setup

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+import tinymce
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=180, null=False)
+    text = tinymce.models.HTMLField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField()
+    last_modified_date = models.DateTimeField()
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def last_modified(self):
+        self.last_modified_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title, self.author
