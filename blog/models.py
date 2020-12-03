83c99b8c92bac604845537a08ba72f3117e39274
Dhruv <dhruv.agarwal2173@gmail.com>
2020-12-03 18:38:49 +0530

    Added a publish button in the draft view which publishes the blog and then redirects to the post detail view  feature

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title, self.author
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
