144ff0113da6d23999fef5d472485f36a1bb3e11
Dhruv <dhruv.agarwal2173@gmail.com>
2020-12-03 13:48:11 +0530

    Fixed some typos and bugs

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=180, null=False)
     text = tinymce.models.HTMLField()
     created_date = models.DateTimeField(auto_now_add=True, editable=False)
-    last_modified_date = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title, self.author
